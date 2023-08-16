8b8302a4dbd4546eef8409996f3a21f5957445da
Pallavi <pallavi0204@gmail.com>
2023-08-16 13:17:15 +0200

    FeedLoaderWithFallbackComposite.load delivers primary feed on primary loader success

--- ListSampleApp/ListSampleAppTests/FeedLoaderWithFallbackCompositeTests.swift
+++ ListSampleApp/ListSampleAppTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -6,30 +6,59 @@
 //
 
 import XCTest
-@testable import ListSampleApp
+import EssentialFeed
 
-final class FeedLoaderWithFallbackCompositeTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+class FeedLoaderWithFallbackComposite: FeedLoader {
+    private let feedloader: FeedLoader
+    
+    init(primary: FeedLoader, fallback: FeedLoader) {
+        self.feedloader = primary
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        feedloader.load(completion: completion)
     }
+    
+}
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+
+class FeedLoaderWithFallbackCompositeTests: XCTestCase {
+
+    func test_load_deliveryRemoteFeedOnRemoteSucess(){
+        
+        let primaryFeed = createuniqueFeedImage()
+        let fallbaclFeed = createuniqueFeedImage()
+        let primaryloader = LoaderStub(result: .success(primaryFeed))
+        let fallbackloader = LoaderStub(result: .success(fallbaclFeed))
+        let sut = FeedLoaderWithFallbackComposite(primary: primaryloader, fallback: fallbackloader)
+        
+        let exp = expectation(description: "wait till expection")
+        sut.load { result in
+            switch result {
+            case let .success(receivedFeed):
+                XCTAssertEqual(receivedFeed,primaryFeed)
+            case .failure:
+                XCTFail("Expcted sucessfully load feedresult, got \(result) instead")
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+   
+    private func createuniqueFeedImage() -> [FeedImage] {
+        return [FeedImage(id: UUID(), description: "any", location: nil, imageURL: URL(string: "http://any-url")!)]
+    }
+    
+    private class LoaderStub: FeedLoader {
+        
+        private let result: FeedLoader.Result
+        
+        init(result: FeedLoader.Result) {
+            self.result = result
+        }
+        
+        func load(completion: @escaping (FeedLoader.Result) -> Void) {
+           completion(result)
         }
     }
 
