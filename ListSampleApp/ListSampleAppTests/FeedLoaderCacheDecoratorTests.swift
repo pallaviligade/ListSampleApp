aaa62353bfbe75cedfb139e929cfb160699bc860
Pallavi <pallavi0204@gmail.com>
2023-08-24 14:19:15 +0200

    replace if let with map

--- ListSampleApp/ListSampleAppTests/FeedLoaderCacheDecoratorTests.swift
+++ ListSampleApp/ListSampleAppTests/FeedLoaderCacheDecoratorTests.swift
@@ -27,11 +27,15 @@ final class FeedLoaderCacheDecorator: FeedLoader {
     
     func load(completion: @escaping (FeedLoader.Result) -> Void) {
         decoratee.load { [weak self] result in
-            if let feed = try? result.get() {
-                self?.cache.save(feed, completion: { _ in
-                    completion(result)
-                })
-            }
+//            if let feed = try? result.get() {
+//                self?.cache.save(feed, completion: { _ in
+//                    completion(result)
+//                })
+//            } we can also write above code in below also
+            completion(result.map{ feed in
+                self?.cache.save(feed, completion: { _ in })
+                return feed
+            })
         }
     }
     
