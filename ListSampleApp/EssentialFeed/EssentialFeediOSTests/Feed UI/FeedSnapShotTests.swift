b76e8eefe2fe54920f555985a9f251578b9513be
Pallavi <pallavi0204@gmail.com>
2023-09-12 12:18:27 +0200

    record Feed with content

--- ListSampleApp/EssentialFeed/EssentialFeediOSTests/Feed UI/FeedSnapShotTests.swift
+++ ListSampleApp/EssentialFeed/EssentialFeediOSTests/Feed UI/FeedSnapShotTests.swift
@@ -7,6 +7,7 @@
 
 import XCTest
 import EssentialFeediOS
+@testable import EssentialFeed
 
 class FeedSnapShotTests: XCTestCase {
     
@@ -16,6 +17,14 @@ class FeedSnapShotTests: XCTestCase {
         sut.display(emptyFeed())
         
         record(snapShot: sut.snapshot(), named: "EMPTY_FEED")
+    }
+    
+    func test_FeedWithContent() {
+        let sut = makeSUT()
+        
+        sut.display(feedWithContent())
+        
+        record(snapShot: sut.snapshot(), named: "FEED_WITH_CONTENT")
     }
     
     private func makeSUT() -> FeedViewController {
@@ -29,6 +38,16 @@ class FeedSnapShotTests: XCTestCase {
     private func emptyFeed() -> [FeedImageCellController]{
         return []
     }
+    
+    private func feedWithContent() -> [ImageStub] {
+        
+        let stub = [ImageStub(description: "The East Side Gallery is an open-air gallery in Berlin. It consists of a series of murals painted directly on a 1,316 m long remnant of the Berlin Wall, located near the centre of Berlin, on Mühlenstraße in Friedrichshain-Kreuzberg. The gallery has official status as a Denkmal, or heritage-protected landmark.",
+                              location: "East Side Gallery\nMemorial in Berlin, Germany",
+                              image: UIImage.make(withColor: .systemPink)),
+            ImageStub(description: "The East Side Gallery is an open-air gallery in Berlin. It consists of a series of murals painted directly on a 1,316 m long remnant of the Berlin Wall, located near the centre of Berlin, on Mühlenstraße in Friedrichshain-Kreuzberg. The gallery has official status as a Denkmal, or heritage-protected landmark.", location: "East Side Gallery\nMemorial in Berlin, Germany", image: UIImage.make(withColor: .systemPink))]
+        
+        return stub
+     }
     
     private func record(snapShot: UIImage, named name: String, file: StaticString = #file, line: UInt = #line) {
         
@@ -64,4 +83,43 @@ extension UIViewController {
             view.layer.render(in: action.cgContext)
         }
     }
+}
+
+private extension FeedViewController {
+    func display(_ stubs: [ImageStub]) {
+        let cells: [FeedImageCellController] = stubs.map({ stub in
+            let cellController = FeedImageCellController(delegate: stub)
+            stub.controller = cellController
+            return cellController
+        })
+        
+        display(cells)
+    }
+}
+
+private class ImageStub: FeedImageCellControllerDelegate {
+   
+    weak var controller: FeedImageCellController?
+    let viewModel:  FeedImageViewModel<UIImage>
+    
+    init(description: String?, location: String?, image: UIImage?) {
+        viewModel = FeedImageViewModel(description: description,
+                                       location: location,
+                                       image: image,
+                                       isLoading: false,
+                                       shouldRetry: image == nil)
+        
+    }
+    
+    
+    
+    func didRequestImage() {
+        controller?.display(viewModel)
+    }
+    
+    func didCancelImageRequest() {
+        
+    }
+    
+    
 }
