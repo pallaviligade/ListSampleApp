83df1abecdeeb9229d7d1821cbed661e2a8648c2
Pallavi <pallavi0204@gmail.com>
2023-09-18 14:42:55 +0200

    SceneDelegate : Used combine framework for binding and used publisher

--- ListSampleApp/ListSampleApp/SceneDelegate.swift
+++ ListSampleApp/ListSampleApp/SceneDelegate.swift
@@ -9,6 +9,7 @@ import UIKit
 import EssentialFeed
 import EssentialFeediOS
 import CoreData
+import Combine
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -73,9 +74,20 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneWillResignActive(_ scene: UIScene) {
         localFeedLoder.validateCache(completion: {_ in })
     }
-//    func makeRemoteClient() -> Httpclient{
-//        return httpClient
-//    }
+    
+    private func makeRemoteFeedLoaderWithLocalFallback() -> AnyPublisher<[FeedImage], Error>
+    {
+        let remoteURL = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
+        
+        let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: httpClient)
+        
+        return Deferred {
+            Future  { completion in
+                remoteFeedLoader.load(completion: completion)
+            }
+        }.eraseToAnyPublisher() // Used when we wants to hide Publisher
+    }
+
 }
 
 
