69dbf57b15e9b20430ce2168ff4ac5c5640ab012
Pallavi <pallavi0204@gmail.com>
2023-09-18 15:03:32 +0200

    created extensions and typalise

--- ListSampleApp/ListSampleApp/SceneDelegate.swift
+++ ListSampleApp/ListSampleApp/SceneDelegate.swift
@@ -75,17 +75,24 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         localFeedLoder.validateCache(completion: {_ in })
     }
     
-    private func makeRemoteFeedLoaderWithLocalFallback() -> AnyPublisher<[FeedImage], Error>
+    private func makeRemoteFeedLoaderWithLocalFallback() -> RemoteFeedLoader.Publisher
     {
         let remoteURL = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
         
         let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: httpClient)
+       return remoteFeedLoader.loadPublisher()
         
-        return Deferred {
-            Future  (remoteFeedLoader.load)
-        }.eraseToAnyPublisher() // Used when we wants to hide Publisher
     }
 
+}
+
+extension RemoteFeedLoader {
+    typealias Publisher = AnyPublisher<[FeedImage], Swift.Error>
+    func loadPublisher() -> Publisher {
+        Deferred {
+            Future(self.load(completion:))
+        }.eraseToAnyPublisher()
+    }
 }
 
 
