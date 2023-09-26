3d4643636bba072e9d8d4caf6558324b0dde7671
Pallavi <pallavi0204@gmail.com>
2023-09-26 13:34:26 +0200

    Moved ImageComment Mapper to separate file

--- ListSampleApp/EssentialFeed/EssentialFeed/Image Comments API/ImageCommentsMapper.swift
+++ ListSampleApp/EssentialFeed/EssentialFeed/Image Comments API/ImageCommentsMapper.swift
@@ -6,5 +6,19 @@
 //
 
 import Foundation
+final class ImageCommentMapper {
+    
+    private struct Root: Decodable {
+        let items: [RemoteFeedItem]
+    }
+    
+    static func map(_ data: Data, response: HTTPURLResponse) throws ->  [RemoteFeedItem] {
+         guard let root = try? JSONDecoder().decode(Root.self, from: data) else {
+           throw RemoteFeedLoader.Error.invaildData
+        }
+        return root.items
+    }
+    
+}
 
 
--- ListSampleApp/EssentialFeed/EssentialFeed/Image Comments API/RemoteImageCommentsLoader.swift
+++ ListSampleApp/EssentialFeed/EssentialFeed/Image Comments API/RemoteImageCommentsLoader.swift
@@ -55,20 +55,3 @@ private extension Array where Element == RemoteFeedItem {
         return map({ FeedImage(id: $0.id, description: $0.description, location: $0.location, imageURL: $0.image) })
     }
 }
-
-final class ImageCommentMapper {
-    
-    private struct Root: Decodable {
-        let items: [RemoteFeedItem]
-    }
-    
-    static func map(_ data: Data, response: HTTPURLResponse) throws ->  [RemoteFeedItem] {
-         guard let root = try? JSONDecoder().decode(Root.self, from: data) else {
-           throw RemoteFeedLoader.Error.invaildData
-        }
-        return root.items
-    }
-    
-}
-
-
