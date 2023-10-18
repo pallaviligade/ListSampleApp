1e2e5e9f8dd9fe97faf94dd17d401aa935f4cbb6
Pallavi <pallavi0204@gmail.com>
2023-10-18 15:41:23 +0200

    inline params

--- ListSampleApp/EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTest.swift
+++ ListSampleApp/EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTest.swift
@@ -22,8 +22,9 @@ class FeedPresenterTest: XCTestCase{
     }
     //MARK: - Helpers
        
-    private func localized(_ key: String,table: String = "Feed", file: StaticString = #file, line: UInt = #line) -> String {
-            let bundle = Bundle(for: FeedPresenter.self)
+    private func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
+           let  table = "Feed"
+           let bundle = Bundle(for: FeedPresenter.self)
             let value = bundle.localizedString(forKey: key, value: nil, table: table)
             if value == key {
                 XCTFail("Missing localized string for key: \(key) in table: \(table)", file: file, line: line)
