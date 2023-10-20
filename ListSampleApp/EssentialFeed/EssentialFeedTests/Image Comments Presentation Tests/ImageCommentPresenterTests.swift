135e54bb0b0df09218fc5f7e66bbe284d163aa16
Pallavi <pallavi0204@gmail.com>
2023-10-20 10:30:21 +0200

    Inject calendar to make tests reliable

--- ListSampleApp/EssentialFeed/EssentialFeedTests/Image Comments Presentation Tests/ImageCommentPresenterTests.swift
+++ ListSampleApp/EssentialFeed/EssentialFeedTests/Image Comments Presentation Tests/ImageCommentPresenterTests.swift
@@ -34,11 +34,11 @@ class ImageCommentPresenterTests: XCTestCase {
         
         let comments = [ImageComment(id: UUID(),
                                      message: "a message",
-                                     createdAt: now.adding(minutes: -5),
+                                     createdAt: now.adding(minutes: -5, calendar: calendar),
                                      username: "a username"),
                         ImageComment(id: UUID(),
                                      message: "a another message",
-                                     createdAt: now.adding(days: -1),
+                                     createdAt: now.adding(days: -1, calendar: calendar),
                                      username: "another username")
         ]
         
