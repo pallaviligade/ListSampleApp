79cca49bb12165e093f8773f2fd5f94ca9d0505b
Pallavi <pallavi0204@gmail.com>
2023-11-10 14:52:21 +0100

    adding changes

--- ListSampleApp/EssentialFeed/EssentialFeediOS/Shared  UI/Controller/ListViewController.swift
+++ ListSampleApp/EssentialFeed/EssentialFeediOS/Shared  UI/Controller/ListViewController.swift
@@ -8,41 +8,34 @@
 import UIKit
 import EssentialFeed
 
-
-
-
-//public protocol CellController {
-//    func view(at tableView: UITableView) -> UITableViewCell // This func creates view for each cell
-//    func preload()
-//    func cancelLoad()
-//}
-//
-//public extension CellController {
-//    func preload() {}
-//    func cancelLoad() {}
-//}
-
-public final  class ListViewController: UITableViewController,UITableViewDataSourcePrefetching, ResourceLoadingView, ResourceErrorView
-{
-   
-    private(set) public var errorView =  ErrorView()
-    public var onRefresh: (() -> Void)?
+public final class ListViewController: UITableViewController, UITableViewDataSourcePrefetching, ResourceLoadingView, ResourceErrorView {
+    private(set) public var errorView = ErrorView()
     
     private lazy var dataSource: UITableViewDiffableDataSource<Int, CellController> = {
-            .init(tableView: tableView) { (tableView, index, controller) in
-                print("\(index.row)")
-                return controller.dataSource.tableView(tableView, cellForRowAt: index)
-               
-            }
-        }()
-  
-    private var onViewIsAppearing: ((ListViewController) -> Void)?
+        .init(tableView: tableView) { (tableView, index, controller) in
+            controller.dataSource.tableView(tableView, cellForRowAt: index)
+        }
+    }()
+    
+    public var onRefresh: (() -> Void)?
     
     public override func viewDidLoad() {
         super.viewDidLoad()
+        
         configureTableView()
-        self.tableView.dataSource = dataSource
         refresh()
+    }
+    
+    private func configureTableView() {
+        dataSource.defaultRowAnimation = .fade
+        tableView.dataSource = dataSource
+        tableView.tableHeaderView = errorView.makeContainer()
+        
+        errorView.onHide = { [weak self] in
+            self?.tableView.beginUpdates()
+            self?.tableView.sizeTableHeaderToFit()
+            self?.tableView.endUpdates()
+        }
     }
     
     public override func viewDidLayoutSubviews() {
@@ -51,53 +44,29 @@ public final  class ListViewController: UITableViewController,UITableViewDataSou
         tableView.sizeTableHeaderToFit()
     }
     
-    private func configureTableView() {
-            dataSource.defaultRowAnimation = .fade
-            tableView.dataSource = dataSource
-            tableView.tableHeaderView = errorView.makeContainer()
-                        
-            errorView.onHide = { [weak self] in
-                self?.tableView.beginUpdates()
-                self?.tableView.sizeTableHeaderToFit()
-                self?.tableView.endUpdates()
-            }
-    }
-    
-  
-  
-   
     public override func traitCollectionDidChange(_ previous: UITraitCollection?) {
-            if previous?.preferredContentSizeCategory != traitCollection.preferredContentSizeCategory {
-                tableView.reloadData()
-            }
+        if previous?.preferredContentSizeCategory != traitCollection.preferredContentSizeCategory {
+            tableView.reloadData()
         }
-    @IBAction func refresh()
-    {
+    }
+    
+    @IBAction private func refresh() {
         onRefresh?()
-        
     }
-    
-    public var errorMessage: String? {
-        return errorView.message
-    }
-    
-   
     
     public func display(_ sections: [CellController]...) {
-            var snapshot = NSDiffableDataSourceSnapshot<Int, CellController>()
-            sections.enumerated().forEach { section, cellControllers in
-                snapshot.appendSections([section])
-                snapshot.appendItems(cellControllers, toSection: section)
-            }
-
-            if #available(iOS 15.0, *) {
-                dataSource.applySnapshotUsingReloadData(snapshot)
-            } else {
-                dataSource.apply(snapshot)
-            }
+        var snapshot = NSDiffableDataSourceSnapshot<Int, CellController>()
+        sections.enumerated().forEach { section, cellControllers in
+            snapshot.appendSections([section])
+            snapshot.appendItems(cellControllers, toSection: section)
         }
-    
-    // Every time we get new model we so reseting it loadingController
+        
+        if #available(iOS 15.0, *) {
+            dataSource.applySnapshotUsingReloadData(snapshot)
+        } else {
+            dataSource.apply(snapshot)
+        }
+    }
     
     public func display(_ viewModel: ResourceLoadingViewModel) {
         refreshControl?.update(isRefreshing: viewModel.isLoading)
@@ -108,77 +77,35 @@ public final  class ListViewController: UITableViewController,UITableViewDataSou
     }
     
     public override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let dl = cellController(at: indexPath)?.delgate
+        let dl = cellController(at: indexPath)?.delegate
         dl?.tableView?(tableView, didSelectRowAt: indexPath)
     }
     
     public override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        let dl = cellController(at: indexPath)?.delgate
+        let dl = cellController(at: indexPath)?.delegate
         dl?.tableView?(tableView, willDisplay: cell, forRowAt: indexPath)
     }
     
     public override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-      // cancelCellControllerLoads(forRowAt: indexPath)
-        let dl = cellController(at: indexPath)?.delgate
+        let dl = cellController(at: indexPath)?.delegate
         dl?.tableView?(tableView, didEndDisplaying: cell, forRowAt: indexPath)
     }
     
     public func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
         indexPaths.forEach { indexPath in
-            let dsp = cellController(at: indexPath)?.dataSourcePrefetch
+            let dsp = cellController(at: indexPath)?.dataSourcePrefetching
             dsp?.tableView(tableView, prefetchRowsAt: [indexPath])
         }
     }
     
     public func tableView(_ tableView: UITableView, cancelPrefetchingForRowsAt indexPaths: [IndexPath]) {
         indexPaths.forEach { indexPath in
-            let dsp = cellController(at: indexPath)?.dataSourcePrefetch
+            let dsp = cellController(at: indexPath)?.dataSourcePrefetching
             dsp?.tableView?(tableView, cancelPrefetchingForRowsAt: [indexPath])
         }
-        
-    }
-    private func cellController(at indexPath: IndexPath) -> CellController? {
-        dataSource.itemIdentifier(for: indexPath)
-    }
-  /*  private func cellController(forRowAt indexPath: IndexPath) -> CellController {
-        //return tableModel[indexPath.row]
-        
-        let controller = tableModel[indexPath.row]
-        loadingController[indexPath] = controller
-        return controller
     }
     
-    private func removeloadingController(forRowAt indexPath: IndexPath) -> CellController? {
-        let controller = loadingController[indexPath]
-        loadingController[indexPath] = nil
-        return controller
-    }
-   private func configureErrorView() {
-          let container = UIView()
-          container.backgroundColor = .clear
-          container.addSubview(errorView)
-
-          errorView.translatesAutoresizingMaskIntoConstraints = false
-          NSLayoutConstraint.activate([
-              errorView.leadingAnchor.constraint(equalTo: container.leadingAnchor),
-              container.trailingAnchor.constraint(equalTo: errorView.trailingAnchor),
-              errorView.topAnchor.constraint(equalTo: container.topAnchor),
-              container.bottomAnchor.constraint(equalTo: errorView.bottomAnchor),
-          ])
-
-          tableView.tableHeaderView = container
-
-          errorView.onHide = { [weak self] in
-              self?.tableView.beginUpdates()
-              self?.tableView.sizeTableHeaderToFit()
-              self?.tableView.endUpdates()
-          }
-      }
-   */
-}
-
-extension UIRefreshControl {
-    func update(isRefreshing: Bool) {
-        isRefreshing ? beginRefreshing() : endRefreshing()
+    private func cellController(at indexPath: IndexPath) -> CellController? {
+        dataSource.itemIdentifier(for: indexPath)
     }
 }
---
+++ ListSampleApp/EssentialFeed/EssentialFeediOS/Shared  UI/Views/Helpers/UIRefreshControl+TestHelper.swift
@@ -0,0 +1,8 @@
+//
+//  UIRefreshControl+TestHelper.swift
+//  EssentialFeediOS
+//
+//  Created by Pallavi on 09.11.23.
+//
+
+import Foundation
