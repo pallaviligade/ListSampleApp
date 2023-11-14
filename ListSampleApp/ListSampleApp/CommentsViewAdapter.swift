8f7d71045e2bbc262db86841eb0b63c2f39d063c
Pallavi <pallavi0204@gmail.com>
2023-11-14 13:43:48 +0100

    pusing changes again

--- ListSampleApp/ListSampleApp/CommentsViewAdapter.swift
+++ ListSampleApp/ListSampleApp/CommentsViewAdapter.swift
@@ -5,4 +5,23 @@
 //  Created by Pallavi on 14.11.23.
 //
 
-import Foundation
+import UIKit
+import EssentialFeed
+import EssentialFeediOS
+
+public class CommentsViewAdapter: ResourceView {
+    public typealias ResourceViewModel = ImageCommentsViewModel
+
+    private weak var controller: ListViewController?
+
+    init(controller: ListViewController) {
+        self.controller = controller
+    }
+
+    public func display(_ viewModel: ImageCommentsViewModel) {
+        controller?.display(viewModel.comment.map {
+            CellController(id: $0, ImageCommentCellController(model: $0))
+        })
+    }
+}
+
