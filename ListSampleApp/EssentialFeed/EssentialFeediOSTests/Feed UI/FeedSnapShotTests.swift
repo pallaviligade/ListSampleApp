25d19100c412568e150b5eb96ec949fd8925151b
Pallavi <pallavi0204@gmail.com>
2023-11-15 17:38:41 +0100

    Add "Load more" message label

--- ListSampleApp/EssentialFeed/EssentialFeediOSTests/Feed UI/FeedSnapShotTests.swift
+++ ListSampleApp/EssentialFeed/EssentialFeediOSTests/Feed UI/FeedSnapShotTests.swift
@@ -48,6 +48,15 @@ class FeedSnapShotTests: XCTestCase {
         assert(snapshot: sut.snapshot(for: .iPhone(style: .dark)), named: "FEED_WITH_LOAD_MORE_INDICATOR_dark")
     }
     
+    func test_feedWithLoadMoreError() {
+        let sut = makeSUT()
+        
+        sut.display(feedWithLoadMoreError())
+        
+        assert(snapshot: sut.snapshot(for: .iPhone(style: .light)), named: "FEED_WITH_LOAD_MORE_ERROR_light")
+        assert(snapshot: sut.snapshot(for: .iPhone(style: .dark)), named: "FEED_WITH_LOAD_MORE_ERROR_dark")
+        assert(snapshot: sut.snapshot(for: .iPhone(style: .light, contentSize: .extraExtraExtraLarge)), named: "FEED_WITH_LOAD_MORE_ERROR_extraExtraExtraLarge")
+    }
     
     private func makeSUT() -> ListViewController {
         let bundle = Bundle(for: ListViewController.self)
@@ -80,12 +89,24 @@ class FeedSnapShotTests: XCTestCase {
     }
     
     private func feedWithLoadMoreIndicator() -> [CellController] {
-        let stub = feedWithContent().last!
-        let cellController = FeedImageCellController(viewModel: stub.viewModel, delegate: stub, selection: {})
-        stub.controller = cellController
+        
         
         let loadMore = LoadMoreCellController()
         loadMore.display(ResourceLoadingViewModel(isLoading: true))
+        return feedWith(loadMore: loadMore)
+    }
+    
+    private func feedWithLoadMoreError() -> [CellController] {
+        
+        let loadMore = LoadMoreCellController()
+        loadMore.display(ResourceErrorViewModel(message: "This is a multiline\nerror message"))
+        return feedWith(loadMore: loadMore)
+    }
+    
+    private func feedWith(loadMore: LoadMoreCellController) ->  [CellController] {
+        let stub = feedWithContent().last!
+        let cellController = FeedImageCellController(viewModel: stub.viewModel, delegate: stub, selection: {})
+        stub.controller = cellController
         return [
             CellController(id: UUID(), loadMore),
             CellController(id: UUID(), cellController)
