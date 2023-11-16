4d11100cd301d782a9437c6b78f14d6972ccd3d7
Pallavi <pallavi0204@gmail.com>
2023-11-16 12:18:47 +0100

    Add assertionFailure to catch CoreData issues in Debug builds

--- ListSampleApp/ListSampleApp/NullStore.swift
+++ ListSampleApp/ListSampleApp/NullStore.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import EssentialFeed
+
+class NullStore: FeedStore & FeedImageDataStore {
+    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        completion(.success(()))
+    }
+
+    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        completion(.success(()))
+    }
+
+    func retrieve(completion: @escaping RetrievalCompletion) {
+        completion(.success(.none))
+    }
+
+    func insert(_ data: Data, for url: URL, completionHandler completion: @escaping (InsertionResult) -> Void) {
+        completion(.success(()))
+    }
+
+    func retrieve(dataForUrl url: URL, completionHandler completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
+        completion(.success(.none))
+    }
+}
