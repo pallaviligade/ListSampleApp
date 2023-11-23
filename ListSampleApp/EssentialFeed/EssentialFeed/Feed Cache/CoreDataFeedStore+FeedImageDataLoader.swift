eb3d5ee92107182a52c7272aa51eb46c5b7967aa
Pallavi <pallavi0204@gmail.com>
2023-11-23 08:46:22 +0100

    Make CoreData FeedImageDataStore implementation sync

--- ListSampleApp/EssentialFeed/EssentialFeed/Feed Cache/CoreDataFeedStore+FeedImageDataLoader.swift
+++ ListSampleApp/EssentialFeed/EssentialFeed/Feed Cache/CoreDataFeedStore+FeedImageDataLoader.swift
@@ -10,22 +10,22 @@ import Foundation
 
 extension CoreDataFeedStore: FeedImageDataStore {
 
-    public func insert(_ data: Data, for url: URL, completionHandler: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
-        performAsync { context in
-                completionHandler(Result {
+    public func insert(_ data: Data, for url: URL) throws {
+       try performAsync { context in
+                Result {
                     try ManagedFeedImage.first(with: url, in: context)
                                         .map { $0.data = data }
                                         .map(context.save)
-                })
+                }
             }
         }
 
-    public func retrieve(dataForUrl url: URL, completionHandler completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
-        performAsync { context in
-                    completion(Result {
-                        try ManagedFeedImage.data(with: url, in: context)
-                    })
+    public func retrieve(dataForURL url: URL) throws -> Data? {
+            try performSync { context in
+                Result {
+                    try ManagedFeedImage.data(with: url, in: context)
                 }
-    }
+            }
+        }
 
 }
